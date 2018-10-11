05c3c78bac86ab5b4305bb2fce390bda3ad21113
RED_EYE <med45c@gmail.com>
2018-10-11 13:37:55 +0300

    Fucking pdf with it's chars

--- DataSheetManager.py
+++ DataSheetManager.py
@@ -29,7 +29,7 @@ class DataSheetManager:
         'MK': (MKM_DATASHEET_URL, MK_DataSheet),
     }
 
-    def __init__(self, datasheets: List[str]) ->None:
+    def __init__(self, datasheets: List[str]) -> None:
         self.datasheets = datasheets
         self.datasheets_datasheets = {}
 
@@ -37,42 +37,48 @@ class DataSheetManager:
         for loader in sorted(self.DATASHEET_URLS, key=lambda l: len(l), reverse=True):
             if loader.upper() in mc.upper():
                 return loader, self.DATASHEET_URLS[loader]
-        return None, (None,None)
+        return None, (None, None)
 
     def get_or_download(self):
         for controller in tqdm(self.datasheets):
             known_controller, (url, datasheet_loader) = self.get_datasheet_loader(controller)
-            if known_controller.upper() in controller.upper():
-                # if known_controller == 'MKL' or (known_controller == 'MK' and known_controller!='MKM'):
-                #     path = Path('./') / 'Datasheets' / known_controller / "{}.pdf".format(known_controller)
-                # else:
-                path = Path('./') / 'Datasheets' / known_controller / "{}.pdf".format(controller)
-                path = path.absolute()
-                if not path.parent.exists():
-                    path.parent.mkdir(exist_ok=True)
-                if path.exists():
-                    datasheet = datasheet_loader(str(path))
-                else:
-                    if self.get_datasheet_loader(controller)[0] == 'MK':
-                        print('CAN\'T DOWNLOAD NXP DATASHEETS AUTOMATICALLY', file=sys.stderr)
-                        print('PLEASE ADD {} DATASHEET MANUALLY!'.format(controller), file=sys.stderr)
-                        continue
-                    print(controller, ' is unknown , trying to download datasheet')
-                    r = requests.get(url.format(controller), stream=True)
-                    if r.status_code == 200:
-                        os.makedirs(path.parent, exist_ok=True)
-                        with path.open('wb') as f:
-                            total_length = int(r.headers.get('content-length'))
-                            for chunk in tqdm(r.iter_content(chunk_size=1024), total=int(total_length / 1024) + 1,
-                                              unit='Kbit'):
-                                if chunk:
-                                    f.write(chunk)
-                                    f.flush()
-                            f.close()
+            if known_controller:
+
+                if known_controller.upper() in controller.upper():
+                    # if known_controller == 'MKL' or (known_controller == 'MK' and known_controller!='MKM'):
+                    #     path = Path('./') / 'Datasheets' / known_controller / "{}.pdf".format(known_controller)
+                    # else:
+                    path = Path('./') / 'Datasheets' / known_controller / "{}.pdf".format(controller)
+                    path = path.absolute()
+                    if not path.parent.exists():
+                        path.parent.mkdir(exist_ok=True)
+                    if path.exists():
                         datasheet = datasheet_loader(str(path))
                     else:
-                        raise Exception('Invalid controller name')
-                self.datasheets_datasheets[controller.upper()] = datasheet
+                        if self.get_datasheet_loader(controller)[0] == 'MK':
+                            print('CAN\'T DOWNLOAD NXP DATASHEETS AUTOMATICALLY', file=sys.stderr)
+                            print('PLEASE ADD {} DATASHEET MANUALLY!'.format(controller), file=sys.stderr)
+                            continue
+                        print(controller, ' is unknown , trying to download datasheet')
+                        r = requests.get(url.format(controller), stream=True)
+                        if r.status_code == 200:
+                            os.makedirs(path.parent, exist_ok=True)
+                            with path.open('wb') as f:
+                                total_length = int(r.headers.get('content-length'))
+                                for chunk in tqdm(r.iter_content(chunk_size=1024), total=int(total_length / 1024) + 1,
+                                                  unit='Kbit'):
+                                    if chunk:
+                                        f.write(chunk)
+                                        f.flush()
+                                f.close()
+                            datasheet = datasheet_loader(str(path))
+                        else:
+                            raise Exception('Invalid controller name')
+                    self.datasheets_datasheets[controller.upper()] = datasheet
+                else:
+                    raise Exception(
+                        'DATASHEET\LOADER for {} was not found\n'
+                        'Proposal: remove or correct name of it'.format(controller))
 
     def __getitem__(self, item: str):
         return self.datasheets_datasheets.get(item.upper(), None)
